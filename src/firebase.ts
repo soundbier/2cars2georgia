c22950226d67451ddf62c4e006f48c23b1ce71f4
soundbier <soundbier@gmail.com>
2026-08-17 11:29:30 +0200

    Refactor Firebase config to use environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,13 +6,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "MUSTERMUSTERMUSTERMUSTER",
-  authDomain: "cars2georgia.firebaseapp.com",
-  databaseURL: "https://cars2georgia-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "cars2georgia",
-  storageBucket: "cars2georgia.firebasestorage.app",
-  messagingSenderId: "948906992041",
-  appId: "1:948906992041:web:2905a3540f60f442f6a4b8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
